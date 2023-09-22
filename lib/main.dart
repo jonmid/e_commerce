78e5de5e67ad0ad222b787d82ad39afe4614e698
Jhonatan Mideros <10jonmid@gmail.com>
2023-09-22 07:52:00 -0500

    [add] config firebase in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'src/utils/firebase/firebase_options.dart';
 import 'package:e_commerce/src/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
