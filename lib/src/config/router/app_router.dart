3f4d9fe1d41befc8d42bc132987ab98c9a626583
Jhonatan Mideros <10jonmid@gmail.com>
2023-10-06 16:10:05 -0500

    [update] colors, form_container, app_router

--- lib/src/config/router/app_router.dart
+++ lib/src/config/router/app_router.dart
@@ -3,24 +3,36 @@ import 'package:go_router/go_router.dart';
 
 import 'package:e_commerce/src/presentation/views/screens/screens.dart';
 
-final appRouter = GoRouter(routes: [
-  GoRoute(
-    path: '/',
-    builder: (context, state) => const Home(),
-  ),
-  GoRoute(
-    path: '/detail-product',
-    pageBuilder: (context, state) {
-      return CustomTransitionPage(
-        key: state.pageKey,
-        child: const DetailProduct(),
-        transitionsBuilder: (context, animation, secondaryAnimation, child) {
-          return FadeTransition(
-            opacity: CurveTween(curve: Curves.easeInOutCirc).animate(animation),
-            child: child,
-          );
-        },
-      );
-    },
-  ),
-]);
+final appRouter = GoRouter(
+  initialLocation: '/home',
+  routes: [
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => const Login(),
+    ),
+    GoRoute(
+      path: '/sign-up',
+      builder: (context, state) => const SignUp(),
+    ),
+    GoRoute(
+      path: '/home',
+      builder: (context, state) => const Home(),
+    ),
+    GoRoute(
+      path: '/detail-product',
+      pageBuilder: (context, state) {
+        return CustomTransitionPage(
+          key: state.pageKey,
+          child: const DetailProduct(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity:
+                  CurveTween(curve: Curves.easeInOutCirc).animate(animation),
+              child: child,
+            );
+          },
+        );
+      },
+    ),
+  ],
+);
